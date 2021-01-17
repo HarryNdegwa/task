27d601124e2448ce83afa9e6171c16b00b28a0cc
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-17 23:20:32 +0300

    Added custom user to settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -18,6 +18,8 @@ with open("key.txt") as file:
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+AUTH_USER_MODEL = 'task.CustomUser
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
