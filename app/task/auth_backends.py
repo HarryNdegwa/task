d17a30c4b148918f77ee1abecb2d070f1824a96f
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-18 00:35:19 +0300

    Added email auth backend

---
+++ app/task/auth_backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
+
+User = get_user_model()
+
+
+class EmailAuthBackend(BaseBackend):
+
+    def authenticate(self,request,email=None,password=None):
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
+        else:
+            password_valid = check_password(password,user.password)
+            if not password_valid:
+                return None
+            return user
+
+
+    def get_user(self,user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
