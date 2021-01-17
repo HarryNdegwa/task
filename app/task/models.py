c28c218c7bcfcae2d7aee057fe23e7801dc3fd2f
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-17 23:18:32 +0300

    Added custom user model

--- app/task/models.py
+++ app/task/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+from .manager import CustomUserManager
+
+ROLE_CHOICES =(("USER","user"),("ADMIN","admin"))
+
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    role = models.CharField(max_length=5,choices=ROLE_CHOICES)
+    profile = models.ImageField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
