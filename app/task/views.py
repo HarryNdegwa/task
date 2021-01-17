6f981efcafe683046d35c72929d4ceb2a1f8cf25
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-18 01:13:34 +0300

    Added login view

--- app/task/views.py
+++ app/task/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class LoginView(View):
+
+    template_name = "login.html"
+
+    def get(self,request):
+        pass
+
+
+
+    def post(self,request):
+        pass
