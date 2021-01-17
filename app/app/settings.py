9a4112a21d0f124d316ab8f0fdd0018d86396251
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-17 22:03:51 +0300

    Secured secret key

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+with open("key.txt") as file:
+    key = file.read().strip()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!125@16k*vbju564vamqr(a)fmx+--d!bcn^*e))+g$b1*r06f'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
