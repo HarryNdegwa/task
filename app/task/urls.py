e2c719ae1cc20320bc081dfa1e31bc569a05ef4a
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-18 02:29:47 +0300

    Added logout url

--- app/task/urls.py
+++ app/task/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     path("login/",LoginView.as_view(),name="login"),
+    path("logout/",logout_view,name="logout"),
     path("user/admin/",AdminView.as_view(),name="admin"),
     path("home/",HomeView.as_view(),name="home")
 ]
