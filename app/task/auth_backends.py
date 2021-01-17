f18a3a96569c9db881bef775eb0fc4e50d1fd9e4
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-18 00:37:01 +0300

    Added phone auth backend

--- app/task/auth_backends.py
+++ app/task/auth_backends.py
@@ -24,3 +24,27 @@ class EmailAuthBackend(BaseBackend):
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+
+class PhoneAuthBackend(BaseBackend):
+
+    def authenticate(self,request,phone=None,password=None):
+        try:
+            user = User.objects.get(phone=phone)
+        except User.DoesNotExist:
+            return None
+        else:
+            password_valid = check_password(password,user.password)
+            if not password_valid:
+                return None
+            return user
+
+
+    def get_user(self,user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+
