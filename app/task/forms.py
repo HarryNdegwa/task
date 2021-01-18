16b3299944c921b315fabbce994bb7056293797c
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-19 01:55:31 +0300

    Removed unwanted code in user update form

--- app/task/forms.py
+++ app/task/forms.py
@@ -21,12 +21,6 @@ class RegisterForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
 
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-
-        for field_name in ["password1","password2"]:
-            self.fields[field_name].help_text = None
-
     class Meta:
         model = User
         fields = ["name","email","phone","role","profile"]
