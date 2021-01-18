f6eabf0797bd7ade8e198f3180a15aada3939e19
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-19 00:11:04 +0300

    Added admin actions url

--- app/task/urls.py
+++ app/task/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("logout/",logout_view,name="logout"),
     path("register/",SignUpView.as_view(),name="register"),
     path("user/admin/",AdminView.as_view(),name="admin"),
+    path("admin/actions/",AdminActionsView.as_view(),name="admin_add_user"),
     path("profile/",ProfileView.as_view(),name="profile"),
     path("home/",HomeView.as_view(),name="home")
 ]
