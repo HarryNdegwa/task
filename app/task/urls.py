8030a7b28f6ebfed59c3a1a0105f65233b2617a5
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-19 01:13:58 +0300

    Mapped user data in view user template and added edit user link

--- app/task/urls.py
+++ app/task/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("logout/",logout_view,name="logout"),
     path("register/",SignUpView.as_view(),name="register"),
     path("user/admin/",AdminView.as_view(),name="admin"),
+    path("admin/view/user/<int:id>/",AdminViewUser.as_view(),name="admin_view_user"),
     path("admin/create/user/",AdminCreateUserView.as_view(),name="admin_create_user"),
     path("admin/edit/user/<int:id>/",AdminEditUserView.as_view(),name="admin_edit_user"),
     path("admin/delete/user/<int:id>/",AdminDeleteUserView.as_view(),name="admin_delete_user"),
