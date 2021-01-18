e38ce7a9b558069f04f744785e97f5ee49282ae8
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-18 17:51:59 +0300

    Routed register view

--- app/task/urls.py
+++ app/task/urls.py
@@ -6,6 +6,7 @@ from .views import *
 urlpatterns = [
     path("login/",LoginView.as_view(),name="login"),
     path("logout/",logout_view,name="logout"),
+    path("register/",SignUpView.as_view(),name="register"),
     path("user/admin/",AdminView.as_view(),name="admin"),
     path("home/",HomeView.as_view(),name="home")
 ]
