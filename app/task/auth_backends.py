ed8297e9132e976620c81ebaf703bdd4153843c1
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-19 13:08:29 +0300

    Added token authentication backend

--- app/task/auth_backends.py
+++ app/task/auth_backends.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
 
 User = get_user_model()
 
@@ -40,6 +42,38 @@ class PhoneAuthBackend(BaseBackend):
                 return None
             return user
 
+
+    def get_user(self,user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+
+
+class TokenAuthentication(BaseAuthentication):
+
+    def authenticate(self, request):
+
+        """
+            authenticate the request and return a user and token tuple    
+        """
+        auth_header = request.headers.get("Authorization")
+        if auth_header:
+            _,token = auth_header.split(" ")
+            if token:
+                res = User.decode_auth_token(token)
+                if type(res) == int:
+                    user = User.objects.get(id=res)
+                    if user:
+                        return (user,None)
+                else:
+                    raise AuthenticationFailed(res)     
+            else:
+                raise AuthenticationFailed("Invalid Token")
+        raise AuthenticationFailed("Invalid Token")
+
+    
 
     def get_user(self,user_id):
         try:
