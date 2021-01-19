a685a6bc2e7648c6cb7dfbb95c0e3a8827486524
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-19 13:37:00 +0300

    Refactored api sign in view logic

--- app/task/api_views.py
+++ app/task/api_views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from rest_framework import permissions,status
 
 
+from .serializers import UserSerializer
+
+
 
 class SignInView(APIView):
 
@@ -13,4 +16,36 @@ class SignInView(APIView):
     authentication_classes = ()
 
     def post(self,request,format=None):
-        pass
+        
+        data = request.data
+
+        primary = data.get("primary")
+
+        user = None
+
+        if self.check_is_email(primary):
+            user = authenticate(email=primary,password=data.get("password"))
+        else:
+            user = authenticate(phone=primary,password=data.get("password"))
+
+        if not user:
+            return Response({'detail': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+        token = user.encode_auth_token(user.id)
+
+        serialized_user = UserSerializer(user)
+
+        return Response({
+            'user': serialized_user.data,           
+            'token': token.decode(),
+        },status=status.HTTP_200_OK)
+
+
+
+
+    
+
+    def check_is_email(self,e):
+        if "@" in e:
+            return True
+        return False
