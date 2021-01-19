afcfbaef131aac8eac9c7071342666e6ee9da21f
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-19 14:01:53 +0300

    Made phone user field unique

--- app/task/models.py
+++ app/task/models.py
@@ -13,7 +13,7 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
 
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20,unique=True)
     role = models.CharField(max_length=5,choices=ROLE_CHOICES)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
